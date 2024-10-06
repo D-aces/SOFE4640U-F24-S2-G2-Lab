747c694a56e9eb2d6bad3ad34fd0f3f40ead1122
DemiwizAces <erdmriosnadeau@gmail.com>
2024-10-06 00:09:22 -0400

    Fixed merge wrinkles

--- app/src/main/java/com/example/newnote/NewNote.java
+++ app/src/main/java/com/example/newnote/NewNote.java
@@ -2,19 +2,20 @@ package com.example.newnote;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import com.google.android.material.textfield.TextInputEditText;
 
 public class NewNote extends AppCompatActivity implements ColourSelectionListener {
+    private String noteColour = "white";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +23,8 @@ public class NewNote extends AppCompatActivity implements ColourSelectionListene
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_new_note);
 
-        ImageButton b = findViewById(R.id.newnoteback);
-        b.setOnClickListener(v -> {
+        ImageButton newNoteBack = findViewById(R.id.newnoteback);
+        newNoteBack.setOnClickListener(v -> {
             Intent i = new Intent(NewNote.this, MainActivity.class);
             startActivity(i);
         });
@@ -35,25 +36,25 @@ public class NewNote extends AppCompatActivity implements ColourSelectionListene
             if (!colourOptions.isAdded()) {
                 colourOptions.setColourSelectionListener(NewNote.this);
                 colourOptions.show(getSupportFragmentManager(), "ModalBottomSheet");
-              
+            }
+        });
+
         Button done = findViewById(R.id.donebutton);
         done.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 TextInputEditText title = findViewById(R.id.title);
-                TextInputEditText subTitle = findViewById(R.id.subTitle);
+                TextInputEditText subTitle = findViewById(R.id.subtitle);
                 TextInputEditText body = findViewById(R.id.body);
 
-                String a = title.getText().toString();
-                String b = subTitle.getText().toString();
-                String c = body.getText().toString();
-                String d = "color";
+                String titleText = title.getText().toString();
+                String subtitleText = subTitle.getText().toString();
+                String bodyText = body.getText().toString();
+                String colour = noteColour;
                 int e = 0;
 
-                DatabaseHandler db = new DatabaseHandler(NewNote.this,null,null,1);
-                db.newNote(a, b, c, d,e);
-
-
+                DatabaseHandler db = new DatabaseHandler(NewNote.this, null, null, 1);
+                db.newNote(titleText, subtitleText, bodyText, colour, e);
 
                 Intent i = new Intent(NewNote.this, MainActivity.class);
                 startActivity(i);
@@ -66,9 +67,7 @@ public class NewNote extends AppCompatActivity implements ColourSelectionListene
             return insets;
         });
     }
-
     public void onColourSelected(int color) {
-        String noteColour = Integer.toHexString(color);
-        System.out.println("The colour is" + noteColour);
+        noteColour = Integer.toHexString(color);
     }
 }
